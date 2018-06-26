ddd998e5a08eda51a6066ce0c77b7359f54928cd
asier <root@P-ASPE.spenta.local>
2018-06-26 11:57:27 +0200

    add courses folders

---
+++ src/js/components/CourseView/CourseView.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import './main.scss'
+
+class CourseView extends Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+  render() {
+    return (
+      <div className='container'>
+          Course View!!!!
+      </div>
+    );
+  }
+}
+export default CourseView;
