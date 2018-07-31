e0f95c4cfe65f728e095408f8cadb834249c2644
Robert Vera <robert@Roberts-iMac.home>
2018-07-31 23:26:47 +0200

    admin views

--- courses-site-app/src/App.js
+++ courses-site-app/src/App.js
@@ -5,6 +5,10 @@ import TopBar from "./js/components/TopBar/TopBar";
 import Home from "./js/components/Home/Home";
 import Footer from "./js/components/Footer/Footer"
 import Contact from "./js/components/Contact/Contact"
+import AdminFrame from "./js/components/Admin/AdminFrame/AdminFrame"
+import Students from "./js/components/Admin/Students/Students"
+import Teachers from "./js/components/Admin/Teachers/Teachers"
+import Courses from "./js/components/Admin/Courses/Courses"
 
 class App extends Component {
   constructor() {
@@ -16,15 +20,32 @@ class App extends Component {
     return (
         <HashRouter>
           <div>
-            <TopBar/>
-            <Route exact path="/" render={() => (
-                  <Home />
-                )} />
-            <Route exact path="/contacto" render={() => (
-                  <Contact />
-                )} />
-            <Footer/>
-            </div>
+            <Route path="/es" render={()=> (
+              <TopBar/>              
+            )} />
+            <Route exact path="/es/home" render={() => (
+              <Home />
+            )} />
+            <Route exact path="/es/contacto" render={() => (
+              <Contact />
+            )} />
+            <Route path="/es" render={()=> (
+              <Footer/>              
+            )} />
+            <Route path="/admin" render={() => (
+              <AdminFrame />
+            )} />
+            <Route exact path="/admin/students" render={() => (
+              <Students />
+            )} />
+            <Route exact path="/admin/teachers" render={() => (
+              <Teachers />
+            )} />
+            <Route exact path="/admin/courses" render={() => (
+              <Courses />
+            )} />
+                         
+            </div>   
         </HashRouter>
     );
   }
