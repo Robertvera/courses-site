6cd938b30ce35b5f7a9da86a231aa894a27b2ce4
Asier <asierp.dev@gmail.com>
2018-08-23 17:18:18 +0200

    Refactor buttons into components

---
+++ courses-site-app/src/js/components/Buttons/deleteButton.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+const deleteButton = () => {
+  return (
+    <button type="button" class="btn-sm btn-outline-danger">Borrar</button>
+  )
+}
+
+export default deleteButton
