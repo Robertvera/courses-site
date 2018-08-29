35b8612db07906f766ce39b0655cbd96d5c39cbe
Asier <asierp.dev@gmail.com>
2018-08-29 18:46:32 +0200

    Call to get saved courses

--- courses-site-app/src/js/components/Admin/Courses/Courses.js
+++ courses-site-app/src/js/components/Admin/Courses/Courses.js
@@ -2,15 +2,32 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import DeleteButton from "../../Buttons/deleteButton"
 import EditButton from "../../Buttons/editButton"
+import Api from '../../../../api/vmApi'
 
 
 class Courses extends Component {
   constructor() {
     super();
     this.state = {
+      courses: ''
     };
   }
+
+  componentDidMount() {
+    this.getCourses()
+  }
+
+  getCourses = () => {
+    Api.listCourses().then(courses => {
+      this.setState({
+        courses: courses.data.data
+      })
+    })
+  }
+
   render() {
+    const { courses } = this.state
+    console.log(courses)
     return (
         <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
   <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
