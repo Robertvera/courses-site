98021d48aacdb9f98c47265f1e81ddbe1ea02a57
Robert Vera <robertveraroige@gmail.com>
2018-11-10 18:35:53 +0100

    add linter

--- courses-site-app/src/js/components/utils/utils.js
+++ courses-site-app/src/js/components/utils/utils.js
@@ -1,42 +1,40 @@
 const Utils = {
-	validateData: function (data) {
-		let missing
-		Object.keys(data).forEach(function(key) {
-			if (!data[key] && !missing) {
-				missing = key
-			}
-		})
+  validateData: function(data) {
+    let missing;
+    Object.keys(data).forEach(function(key) {
+      if (!data[key] && !missing) {
+        missing = key;
+      }
+    });
 
-		return missing ? missing : 'OK'
-	},
+    return missing ? missing : "OK";
+  },
 
-	translateData: function (word) {
-		switch (word) {
-			case 'name': 
-				return 'Nombre'
-			case 'surname':
-				return 'Apellido'
-			case 'dni':
-				return 'DNI'
-			case 'phone':
-				return 'Teléfono'
-			case 'email':
-				return 'Correo electrónico'
-			case 'address':
-				return 'Dirección de Facturación'
-			case 'city':
-				return 'Población'
-			case 'cp':
-				return 'Código Postal'
-		}
-	},
+  translateData: function(word) {
+    switch (word) {
+      case "name":
+        return "Nombre";
+      case "surname":
+        return "Apellido";
+      case "dni":
+        return "DNI";
+      case "phone":
+        return "Teléfono";
+      case "email":
+        return "Correo electrónico";
+      case "address":
+        return "Dirección de Facturación";
+      case "city":
+        return "Población";
+      case "cp":
+        return "Código Postal";
+    }
+  },
 
-	formatDate: function (input) {
-		const newDate = new Date(input)
-		return `${newDate.getDate()}/${newDate.getMonth()+1}/${newDate.getFullYear()}`;
-	}
+  formatDate: function(input) {
+    const newDate = new Date(input);
+    return `${newDate.getDate()}/${newDate.getMonth() + 1}/${newDate.getFullYear()}`;
+  }
+};
 
-
-}
-
-module.exports = Utils
+module.exports = Utils;
