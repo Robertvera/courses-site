9f637f1fcbd068c2ab2bac113aee22210fbfc570
Asier <asierp.dev@gmail.com>
2018-12-08 11:47:16 +0100

    Courses checko for login

--- courses-site-app/src/js/components/Admin/Courses/Courses.js
+++ courses-site-app/src/js/components/Admin/Courses/Courses.js
@@ -4,6 +4,7 @@
 /* eslint-disable react/prop-types */
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import firebase from 'firebase'
 import './Courses.scss'
 import swal from 'sweetalert2';
 import Api from '../../../../api/vmApi';
@@ -14,12 +15,26 @@ class Courses extends Component {
 		super();
 		this.state = {
 			courses: [],
-			skipResults: 0
+      skipResults: 0,
+      login: false
 		};
 	}
 
 	componentDidMount() {
+    this.checkForLogin();
 		this.getCourses();
+	}
+
+	checkForLogin = () => {
+		let userLogin = false
+		firebase.auth().onAuthStateChanged((user) => {
+			if (user) {
+			userLogin = true
+			this.setState({ login: userLogin })
+			} else {
+			this.setState({ login: userLogin })
+			}
+		})
 	}
 
 	getCourses = (remove) => {
@@ -79,11 +94,11 @@ class Courses extends Component {
 	deleteCourse = (e, course) => {
 		e.preventDefault();
 		swal({
-			title: `¿Estás seguro de que quieres eliminar ${ course }?`,
-			text: 'Una vez eliminado, no podrás volver atrás',
+			title: `¿Estás seguro de que qwithRouterar ${ course }?`,
+			text: 'Una vez eliminado, no pwithRouteratrás',
 			type: 'warning',
 			showCancelButton: true,
-			confirmButtonColor: '#3085d6',
+			confirmButtonColor: '#3085d6',withRouter,
 			cancelButtonColor: '#d33',
 			confirmButtonText: 'Sí, lo quiero eliminar',
 			cancelButtonText: 'Cancelar'
@@ -105,87 +120,95 @@ class Courses extends Component {
 	};
 
 	render() {
-		const { courses } = this.state;
+		const { courses, login } = this.state;
 		const { history } = this.props;
 
 
 		return (
-			<main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
-				<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-					<h1 className="h2">Cursos</h1>
-					<input
-						onChange={(e) => this.searchCoursesByQuery(e)}
-						className="form-control query"
-						type="text"
-						placeholder="Search"
-						aria-label="Search"
-					/>
-					<div className="btn-toolbar mb-2 mb-md-0">
-						<div className="btn-group mr-2">
-							<button
-								type="submit"
-								onClick={() => history.push('/admin/courses/manage')}
-								className="btn btn-sm btn-outline-secondary"
-							>
-								{'Crear curso'}
-							</button>
-						</div>
-					</div>
-				</div>
-				<div className="table-responsive">
-					<table className="table table-striped table-sm">
-						<thead>
-							<tr>
-								<th>Nombre</th>
-								<th>Fecha</th>
-								<th>Sede</th>
-								<th>Apuntados</th>
-								<th>Límite</th>
-								<th>Precio</th>
-								<th />
-							</tr>
-						</thead>
-						<tbody>
-							{courses && courses.length
-								? courses.map((course) => (
-									<tr key={course._id}>
-										<td>{course.name}</td>
-										<td>{course.date}</td>
-										<td>{course.location}</td>
-										<td>{course.students.length}</td>
-										<td>{course.capacity}</td>
-										<td>{course.price}</td>
-										<td>
-											<button
-												onClick={(e) => {
-													this.deleteCourse(e, course.name);
-												}}
-												type="button"
-												className="btn-sm btn-outline-danger"
-											>
-												{'Borrar'}
-											</button>
-											<button
-												onClick={(e) => {
-													this.editCourse(e, course.name);
-												}}
-												type="button"
-												className="btn-sm btn-outline-warning"
-											>
-												{'Editar'}
-											</button>
-										</td>
-									</tr>
-								))
-								: null}
-						</tbody>
-					</table>
-					<ShowMoreButton 
-					onClick={(e) => this.showMoreCourses(e)} 
-					type="button" 
-					className="btn-sm btn-outline-danger"/>
-				</div>
-			</main>
+      <React.Fragment>
+        {
+          login ?
+            <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
+              <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 className="h2">Cursos</h1>
+                <input
+                  onChange={(e) => this.searchCoursesByQuery(e)}
+                  className="form-control query"
+                  type="text"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <div className="btn-toolbar mb-2 mb-md-0">
+                  <div className="btn-group mr-2">
+                    <button
+                      type="submit"
+                      onClick={() => history.push('/admin/courses/manage')}
+                      className="btn btn-sm btn-outline-secondary"
+                    >
+                      {'Crear curso'}
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="table-responsive">
+                <table className="table table-striped table-sm">
+                  <thead>
+                    <tr>
+                      <th>Nombre</th>
+                      <th>Fecha</th>
+                      <th>Sede</th>
+                      <th>Apuntados</th>
+                      <th>Límite</th>
+                      <th>Precio</th>
+                      <th />
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {courses && courses.length
+                      ? courses.map((course) => (
+                        <tr key={course._id}>
+                          <td>{course.name}</td>
+                          <td>{course.date}</td>
+                          <td>{course.location}</td>
+                          <td>{course.students.length}</td>
+                          <td>{course.capacity}</td>
+                          <td>{course.price}</td>
+                          <td>
+                            <button
+                              onClick={(e) => {
+                                this.deleteCourse(e, course.name);
+                              }}
+                              type="button"
+                              className="btn-sm btn-outline-danger"
+                            >
+                              {'Borrar'}
+                            </button>
+                            <button
+                              onClick={(e) => {
+                                this.editCourse(e, course.name);
+                              }}
+                              type="button"
+                              className="btn-sm btn-outline-warning"
+                            >
+                              {'Editar'}
+                            </button>
+                          </td>
+                        </tr>
+                      ))
+                      : null}
+                  </tbody>
+                </table>
+                <ShowMoreButton
+                  onClick={(e) => this.showMoreCourses(e)}
+                  type="button"
+                  className="btn-sm btn-outline-danger" />
+              </div>
+            </main>
+            :
+            ''
+        }
+
+      </React.Fragment>
 		);
 	}
 }
